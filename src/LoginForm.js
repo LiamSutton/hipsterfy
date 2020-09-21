280b402a4f59736b4cb854ce7461d955f0cbfd29
Liam Sutton <liamsutton1997@outlook.com>
2020-09-21 19:39:21 +0100

    began to implement LoginForm component

---
+++ src/LoginForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class LoginForm extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div>
+            </div>
+        )
+    }
+}
+
+export default LoginForm;
