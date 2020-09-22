3b504e69deec9213793a6603fe628a26c59e9997
Liam Sutton <liamsutton1997@outlook.com>
2020-09-22 14:51:46 +0100

    Fixed issue with GET request: user details

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
 import React from 'react';
+import queryString from 'query-string'
 import LoginForm from './LoginForm.js'
+import PlaylistCollection from './PlaylistCollection.js';
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super()
 
+     // When the component is initialized check the hash string for the current window 
+     // (if the user is authenticated there will be a token in the hash)
+    let accessToken = queryString.parse(window.location.hash).access_token || null
+
     this.state = {
-      accessToken: null
+      accessToken: accessToken,
+      id: null
     };
   }
-
-  // When the component is initialized check the hash string for the current window (if the user is authenticated there will be a token in the hash)
+ 
   componentDidMount() {
-    let accessToken = window.location.hash.substring(14);
-
-    // This will determine whether a login screen is shown or the application will load
-    if (accessToken != null) {
-      this.setState({
-        accessToken: accessToken
-      });
+    // The application will only request a users details if the user is already authenticated when the app is initialized
+    if (this.state.accessToken !== null) {
+      fetch('https://api.spotify.com/v1/me', {
+        headers: {
+          'Authorization': 'Bearer ' + this.state.accessToken
+        }
+      }).then(response => {
+        response.json().then(data => {
+          this.setState({
+            id: data.id
+          })
+        })
+      })
     }
   }
 
   render() {
-
+     let content = this.state.accessToken ? <PlaylistCollection accessToken={this.state.accessToken} id={this.state.id} /> : <LoginForm />
     return (
       <div className="App">
         <header>
           <h2 className="display-3">H I P S T E R F Y</h2>
         </header>
         <main className="App-Content">
-          <LoginForm />
+          {content}
         </main>
 
         <footer className="App-Footer">
---
+++ src/PlaylistCollection.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+// This component will retrieve and store a list of information regarding a users playlists
+class PlaylistCollection extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            playlists: null,
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default PlaylistCollection
